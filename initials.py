bc1458d473b3a709cb84d7a2c5cd7afa7b88e3ae
clivedenzelmould <2425401085@live.gctu.edu.gh>
2026-05-30 23:56:42 +0000

    2425401085- CLIVE-DENZEL MOULD

--- initials.py
+++ initials.py
@@ -1,7 +1,41 @@
+# File: initials.py
+# Description: Prints stylized large initials CDM with surrounding periods.
+# Assignment Number: 1
+#
+# Name: Clive-Denzel Mould
+# STUDENT ID: 2425401085
+# Email: 2425401085@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Clive-Denzel Mould, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    """Prints the small initials and stylized large initials for CDM."""
+    print()
 
 
+    print("...CDM")
 
-main()
+
+    print()
+
+
+    print("...CCCCCCCCCCCC......DDDDDDDDD...........MMMM....MMMM...")
+    print("...CCCCCCCCCCCC......DDDDDDDDDD..........MMMM....MMMM...")
+    print("...CC................DD.......DDD........MM.MM..MM.MM...")
+    print("...CC................DD........DD........MM.MM..MM.MM...")
+    print("...CC................DD........DD........MM.MM..MM.MM...")
+    print("...CC................DD........DD........MM..MMMM..MM...")
+    print("...CC................DD........DD........MM..MMMM..MM...")
+    print("...CC................DD.......DDD........MM...MM...MM...")
+    print("...CCCCCCCCCCCC...**.DDDDDDDDDD.....**...MM...MM...MM...**")
+    print("...CCCCCCCCCCCC...**.DDDDDDDDD......**...MM...MM...MM...**")
+
+
+    print()
+
+print()
+if __name__ == "__main__":
+    main()
